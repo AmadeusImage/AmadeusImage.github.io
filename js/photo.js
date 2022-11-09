1854d18e67e5f640dad92ab00f0050fc94a3ad4b
Samuel Huang <Samuel_Huang@cyberlink.com>
2022-11-09 12:02:45 +0800

    Site updated: 2022-11-09 12:02:44

--- js/photo.js
+++ js/photo.js
@@ -26,25 +26,24 @@ photo = {
 		//console.log("Hello World");
         var begin = (page - 1) * this.offset;
         var end = page * this.offset;
-		var data_ = data.list[0].arr;
-        if (begin >= data_.link.length) return;
+        if (begin >= data.y.length) return;
         var html, imgNameWithPattern, imgName, imageSize, imageX, imageY, li = "";
 		
         //for (var i = begin; i < end && i < data_.link.length; i++) {
-		for (var i = data_.link.length - 1; i > -1; i--) {
+		for (var i = data.y.length - 1; i > -1; i--) {
 			//if (Math.random() > 0.5)
-			var src = 'https://cdn.jsdelivr.net/gh/AmadeusImage/X@main/photos/' +data_.link[i];
+			var src = 'https://cdn.jsdelivr.net/gh/AmadeusImage/X@main/photos/' +data.fname[i];
 			//else
 			//	src = 'https://raw.githubusercontent.com/AmadeusImage/X/main/photos/' + data.list[0].arr.link[i];
             //imgNameWithPattern = data[i].split(' ')[1];
-            imgName = data_.text[i];//imgNameWithPattern.split('.')[0];
+            imgName = data.prompt[i];//imgNameWithPattern.split('.')[0];
             //imageSize = data[i].split(' ')[0];
             imageX = '512';//imageSize.split('.')[0];
             imageY = '768';//imageSize.split('.')[1];
             li += '<div class="card" style="width:' + imageWidth + 'px" >' +
                     '<div class="ImageInCard" style="height:'+ imageWidth * imageY / imageX + 'px">' +
                       '<a data-fancybox="gallery" href="' + src + '" data-caption="' + imgName + '" title="' +  imgName + '">' +
-                        '<img data-src="' + src + ' " src="' + src + ' " data-loaded="true">' +
+                        '<img class="lozad" data-src="' + src + ' " src="' + src + ' ">' +
                       '</a>' +
                     '</div>' +
                   '</div>';
@@ -53,20 +52,10 @@ photo = {
         $(".ImageGrid").append(li);
 		console.log("Finish append");
 		$(".main-inner").append("<style>.main-inner { width: " + window.innerWidth*0.95 +  "px; }</style>");
-		$(".content.page.posts-expand").append("<style>.content { padding-top: 0px; }</style>");
+        $(".content.page.posts-expand").append("<style>.content { padding-top: 0px; }</style>");
         //this.minigrid();
-		console.log("Finish render");
+        console.log("Finish render");
     },
-    minigrid: function() {
-        var grid = new Minigrid({
-            container: '.ImageGrid',
-            item: '.card',
-            gutter: 12
-        });
-        grid.mount();
-        $(window).resize(function() {
-           grid.mount();
-        });
-    }
+
 }
 photo.init();
